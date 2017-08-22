cb1fbe3e4e3f3e34ae4f05c1608caaa1bbb5430a
oga3999 <zgmf.x09a3999@gmail.com>
2017-08-22 10:54:21 +0000

    first commit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = User.create_unique_string
     super
   end
+
+  # protected
+  # def update_resource(resource, params)
+  #   resource.update_without_current_password(params)
+  # end
 end
